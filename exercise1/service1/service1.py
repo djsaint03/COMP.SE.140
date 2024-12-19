f0a955edb9d948bff4e62d98b12158dbb06f40ad
Johnson Apanishile <djsaint03@gmail.com>
2024-12-20 00:52:49 +0200

    htpasswd was excluded due to ignore cmd thus the htpasswd issue

--- exercise1/service1/service1.py
+++ exercise1/service1/service1.py
@@ -1,10 +1,12 @@
 import os
 import json
 import  requests
+import time
 from flask import Flask
 
 
 app=Flask(__name__)
+sleep_duration = int(os.getenv('SLEEP_DURATION', 0))
 @app.route('/')
 def system_info():
     # Collect information from the container
@@ -16,7 +18,7 @@ def system_info():
     # Get information from Service2
     service2_info = requests.get('http://service2:5000').json()
 
-    return json.dumps({
+    data = {
         "Service1": {
             "IP Address": ip_address,
             "Running Processes": running_processes,
@@ -24,8 +26,26 @@ def system_info():
             "Time Since Last Boot": time_since_boot
         },
         "Service2": service2_info
-    })
+    }
+
+    # Flattening the JSON
+    flat_data = []
+    for service, details in data.items():
+        for key, value in details.items():
+            flat_data.append(f"{service} - {key}: {value}")
+
+    # Display as a single line
+    single_line_result = " ||| ".join(flat_data)
+    print(single_line_result)
+    time.sleep(sleep_duration)
+    return single_line_result
+
+@app.route('/stop', methods=['POST'])
+def stop_containers():
+#still trying to make stop request
+    os.system("kill ")
+    return "Stopping containers...", 200
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8199)
-
